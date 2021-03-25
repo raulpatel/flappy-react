4721ea2bf928b50d596a69948633683b4edc4667
Raul <raul.patel.2008@gmail.com>
2021-03-24 18:56:58 -0700

    buggy when restarting but functioning

--- flappybird/src/App.js
+++ flappybird/src/App.js
@@ -1,14 +1,145 @@
 import React, { Component } from 'react';
-import Game from './Game';
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <Game/>
-        </div>
-    );
-  }
+let maxScore = 0;
+
+export default class App extends Component {
+
+    componentDidMount() {
+        const bird = document.querySelector(".bird");
+        const gameDisplay = document.querySelector(".game-container");
+        const ground = document.querySelector(".ground-moving");
+        const score = document.querySelector(".score");
+
+        let birdLeft = 220;
+        let birdBottom = 100;
+        let gravity = 3;
+        let isGameOver = false;
+        let gap = 430;
+        let currScore = -1;
+
+        function startGame() {
+            birdBottom -= gravity;
+            bird.style.bottom = birdBottom + "px";
+            bird.style.left = birdLeft + "px";
+            score.textContent = "Score:\n" + currScore.toString();
+        }
+        let gameTimerId = setInterval(startGame, 20);
+
+        function control(e) {
+            if (e.keyCode === 32) {
+                jump();
+            }
+        }
+
+        function jump() {
+            if (birdBottom < 500) birdBottom += 55;
+            bird.style.bottom = birdBottom + "px";
+            console.log(birdBottom);
+        }
+        document.addEventListener("keyup", control);
+
+        function generateObstacle() {
+            let obstacleLeft = 500;
+            let obstacleBottom = Math.random() * 60;
+            const obstacle = document.createElement("div");
+            const topObstacle = document.createElement("div");
+            if (!isGameOver) {
+                obstacle.classList.add("obstacle");
+                topObstacle.classList.add("topObstacle");
+            }
+            gameDisplay.appendChild(obstacle);
+            gameDisplay.appendChild(topObstacle);
+            obstacle.style.left = obstacleLeft + "px";
+            topObstacle.style.left = obstacleLeft + "px";
+            obstacle.style.bottom = obstacleBottom + "px";
+            topObstacle.style.bottom = obstacleBottom + gap + "px";
+
+            function moveObstacle() {
+                obstacleLeft -= 2;
+                obstacle.style.left = obstacleLeft + "px";
+                topObstacle.style.left = obstacleLeft + "px";
+
+                if (obstacleLeft === -60) {
+                    clearInterval(timerId);
+                    obstacle.remove();
+                    topObstacle.remove();
+                    //gameDisplay.removeChild(obstacle);
+                    //gameDisplay.removeChild(topObstacle);
+                }
+                if ((obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 && (birdBottom < obstacleBottom + 153
+                    || birdBottom > obstacleBottom + gap - 200)) || birdBottom <= 8) {
+                    clearInterval(timerId);
+                    gameOver();
+                }
+            }
+            let timerId = setInterval(moveObstacle, 20);
+            if (!isGameOver) {
+                setTimeout(generateObstacle, 3000);
+                currScore++;
+                score.textContent = "Score:\n" + currScore.toString();
+            }
+        }
+        generateObstacle();
+        function restartGame(e) {
+            if (e.keyCode === 32) {
+                document.removeEventListener('keyup', restartGame);
+                const gameover = document.querySelector(".game-over");
+                const restart = document.querySelector(".restart");
+                gameover.textContent = "";
+                restart.textContent = "";
+                birdLeft = 220;
+                birdBottom = 100;
+                isGameOver = false;
+                currScore = -1;
+                document.addEventListener("keyup", control);
+                gameTimerId = setInterval(startGame, 20);
+                generateObstacle();
+            }
+        }
+        function gameOver() {
+            clearInterval(gameTimerId);
+            console.log("game over");
+            isGameOver = true;
+            if (currScore > maxScore) maxScore = currScore;
+            score.textContent = "";
+            const gameover = document.querySelector(".game-over");
+            gameover.textContent = "Game Over! \n\n Final Score: \n            " + currScore
+                                    + "\nHigh Score: \n            " + maxScore;
+            const restart = document.querySelector(".restart");
+            restart.textContent = "(press space to restart)";
+            document.removeEventListener("keyup", control);
+            ground.classList.add("ground");
+            ground.classList.remove("ground-moving");
+            const obstacles = document.querySelectorAll(".obstacle");
+            const tops = document.querySelectorAll(".topObstacle");
+            for (let i = 0; i < obstacles.length; i++) {
+                obstacles[i].remove();
+                tops[i].remove();
+            }
+            document.addEventListener('keyup', restartGame);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div class="border-left"></div>
+                <div class="game-container">
+                    <div class="border-top"></div>
+                    <div class="score">
+                        <h2 class="score"></h2>
+                    </div>
+                    <div class="sky">
+                        <h2 class="game-over"></h2>
+                        <p class="restart"></p>
+                        <div class="bird"></div>
+                    </div>
+                </div>
+                <div class="ground-container">
+                    <div class="ground-moving"></div>
+                </div>
+                <div class="border-right"></div>
+            </div>
+        );
+    }
 }
-
-export default App;
